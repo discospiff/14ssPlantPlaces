8a192ff1d015661eaaaf38d546af6c9186b4a1f9
Brandan Jones <brandan.jones@gmail.com>
2014-01-05 14:55:33 -0500

    Initial Commit

---
+++ ss143042prep/src/edu/uc/cech/ss143042prep/PlantDetailFragment.java
@@ -0,0 +1,61 @@
+package edu.uc.cech.ss143042prep;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.uc.cech.ss143042prep.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Plant detail screen.
+ * This fragment is either contained in a {@link PlantListActivity}
+ * in two-pane mode (on tablets) or a {@link PlantDetailActivity}
+ * on handsets.
+ */
+public class PlantDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public PlantDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_plant_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.plant_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
