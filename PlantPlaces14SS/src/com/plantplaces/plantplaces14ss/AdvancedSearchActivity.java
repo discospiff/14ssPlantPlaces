352849869959cbd04fda23d912603d1ecf015ad2
Brandan Jones <brandan.jones@gmail.com>
2014-01-26 22:55:35 -0500

    Passing data from one activity to another Simple if tests

--- PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/AdvancedSearchActivity.java
+++ PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/AdvancedSearchActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 public class AdvancedSearchActivity extends Activity {
 	
+	private AutoCompleteTextView actPlantName;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -14,6 +18,9 @@ public class AdvancedSearchActivity extends Activity {
 		
 		// associate the layout with this activity.
 		setContentView(R.layout.advancedsearch);
+		
+		// get access to the auto complete text where the user will enter a search term.
+		actPlantName = (AutoCompleteTextView) findViewById(R.id.actPlantName);
 	}
 
 	
@@ -24,6 +31,13 @@ public class AdvancedSearchActivity extends Activity {
 	public void searchForPlants(View v) {
 		// create an explicit intent.
 		Intent plantResultsIntent = new Intent(this, PlantResultsActivity.class);
+		
+		// get the data that the user entered into the search field.
+		String searchPlantName = actPlantName.getText().toString();
+		
+		// pass that data to the next activity.
+		plantResultsIntent.putExtra("SEARCH_PLANT_NAME", searchPlantName);
+		
 		// invoke the explicit intent.
 		startActivity(plantResultsIntent);
 	}
--- PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/PlantResultsActivity.java
+++ PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/PlantResultsActivity.java
@@ -9,43 +9,56 @@ import android.widget.ArrayAdapter;
 import com.plantplaces.dto.Plant;
 
 public class PlantResultsActivity extends ListActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		// create a new object from class Plant.
-		Plant redbud = new Plant();
-		redbud.setCommon("Redbud");
-		redbud.setGenus("Cercis");
-		redbud.setSpecies("canadensis");
-		
-		// create a paw paw.
-		Plant pawpaw = new Plant();
-		pawpaw.setCommon("PawPaw");
-		pawpaw.setGenus("Asimina");
-		pawpaw.setSpecies("triloba");
-		
-		// now we have two palnts that we want to display on our results.
-		
+
+		// retrieve any data that was passed in, and was associated with the name "SEARCH_PLANT_NAME"
+		String searchTerm = getIntent().getStringExtra("SEARCH_PLANT_NAME");
+
+
 		// create a collection to hold the plants.
 		ArrayList<Plant> allPlants = new ArrayList<Plant>();
+
+		if (searchTerm.equalsIgnoreCase("redbud")) {
+			// create a new object from class Plant.
+			Plant redbud = new Plant();
+			redbud.setCommon("Redbud");
+			redbud.setGenus("Cercis");
+			redbud.setSpecies("canadensis");
+
+			// add the redbud and the paw paw to this collection.
+			allPlants.add(redbud);
+		}
+
+		if (searchTerm.contains("pawpaw")) {
+			// create a paw paw.
+			Plant pawpaw = new Plant();
+			pawpaw.setCommon("PawPaw");
+			pawpaw.setGenus("Asimina");
+			pawpaw.setSpecies("triloba");
+			allPlants.add(pawpaw);
+		}
 		
-		// add the redbud and the paw paw to this collection.
-		allPlants.add(redbud);
-		allPlants.add(pawpaw);
+		// do we have an empty list?
+		if (allPlants.size() == 0) {
+			Plant empty = new Plant();
+			empty.setCommon("No plants match your result.  Please try again");
+			allPlants.add(empty);
+		}
 		
 		// show this collection in our list.
 		ArrayAdapter<Plant> plantAdapter = new ArrayAdapter<Plant>(this, android.R.layout.simple_list_item_1, allPlants);
-		
+
 		setListAdapter(plantAdapter);
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 	}
 
 }
