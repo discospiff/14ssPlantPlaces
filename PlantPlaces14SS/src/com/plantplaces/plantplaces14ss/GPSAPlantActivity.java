cea8a608b9070a0e1e499cbba48f33141f066c75
Brandan Jones <brandan.jones@gmail.com>
2014-02-02 13:09:46 -0500

    Add implicit intent to invoke Camera.

--- PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/GPSAPlantActivity.java
+++ PlantPlaces14SS/src/com/plantplaces/plantplaces14ss/GPSAPlantActivity.java
@@ -2,20 +2,25 @@ package com.plantplaces.plantplaces14ss;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.plantplaces.dto.Plant;
 
 public class GPSAPlantActivity extends Activity {
 	
+	private static final int CAMERA_RESULT = 5;
 	// declare a variable that will hold a reference to the EditText component on the screen.
 	EditText description;
 	private TextView txtSelectedPlant;
 	private Plant plant;
+	private Bitmap plantImage;
+	private ImageView imgPlant;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,9 @@ public class GPSAPlantActivity extends Activity {
 		
 		// the label that contains the description of the selected plant.
 		txtSelectedPlant = (TextView) findViewById(R.id.txtSelectedPlant);
+		
+		// get a reference to the image view that will display a plant photo.
+		imgPlant = (ImageView) findViewById(R.id.imgPlant);
 	}
 
 	@Override
@@ -57,9 +65,29 @@ public class GPSAPlantActivity extends Activity {
 		
 			/// set this plant in the TextView on the UI.
 			txtSelectedPlant.setText(plant.toString());
+		} else if (requestCode == CAMERA_RESULT) {
+			// we are here because we have received a result from the camera.
+			plantImage = (Bitmap) data.getExtras().get("data");
+			
+			imgPlant.setImageBitmap(plantImage);
 		}
 	}
 
+	/**
+	 * This method will be invoked when the Take Photo button is clicked.
+	 * @param v
+	 */
+	public void onTakePhotoClicked(View v) {
+		// use an implict intent to invoke a camera.
+		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		// start this inent, and anticipate a result.
+		startActivityForResult(cameraIntent, CAMERA_RESULT);
+		
+	}
+	
+	
+	
 }
 
 
